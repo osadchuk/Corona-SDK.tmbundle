9c44248266f7e4e8067322aea2e253cec4608715
Darren Osadchuk <darren@ludicroussoftware.com>
2011-03-02 14:04:19 -0600

    Added snippets for audio methods. Added very basic templates for build.settings, config.lua, and generic modules.

---
+++ Templates/config_lua.tmTemplate/config.lua
@@ -0,0 +1,16 @@
+//
+//  untitled
+//
+//  Created by Darren Osadchuk on 2011-02-03.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+application = {
+	content = {
+		width = 320, height = 480, 
+		scale = "letterbox",
+		imageSuffix = {
+		    ["@2"] = 2,
+		},
+	}
+}
